f91c5ae286824df27dd3c363286ef24fc9f69d34
stavalfi <stavalfi@gmail.com>
2020-06-19 03:58:58 +0300

    improve tests readability

--- packages/tests/e2e/__tests__/utils.ts
+++ packages/tests/e2e/__tests__/utils.ts
@@ -2,6 +2,7 @@ import chance from 'chance'
 import execa from 'execa'
 import got from 'got'
 import { attach, ConnectionFrom, createK8sClient, SingletonStrategy } from 'k8s-api'
+import { randomAppId, subscribe, SubscribeCreatorOptions } from 'k8test'
 
 export const cliMonitoringPath = require.resolve('k8test-cli-logic/dist/src/index.js')
 
@@ -32,55 +33,94 @@ export function cleanupAfterEach() {
   return cleanups
 }
 
-export function prepareEachTest() {
+const startMonitorNamespace = (namespaceName: string) =>
+  execa.command(`node ${cliMonitoringPath} start-monitoring --local-image --namespace ${namespaceName}`, {
+    // eslint-disable-next-line no-process-env
+    env: { ...(process.env['DEBUG'] && { DEBUG: process.env['DEBUG'] }) },
+    stdio: 'inherit',
+  })
+
+const deleteK8testResources = (namespaceName: string) =>
+  execa.command(`node ${cliMonitoringPath} delete-k8test-resources --namespace ${namespaceName}`, {
+    // eslint-disable-next-line no-process-env
+    env: { ...(process.env['DEBUG'] && { DEBUG: process.env['DEBUG'] }) },
+    stdio: 'inherit',
+  })
+
+const attachContainer = async ({
+  appId,
+  imageName,
+  namespaceName,
+  singletonStrategy,
+}: {
+  namespaceName: string
+  appId?: string
+  imageName: string
+  singletonStrategy: SingletonStrategy
+}) => {
+  const k8sClient = createK8sClient(ConnectionFrom.outsideCluster)
+  return await attach({
+    k8sClient,
+    singletonStrategy,
+    appId,
+    imageName,
+    namespaceName,
+  })
+}
+
+const attachMonitoringService = async (namespaceName: string) =>
+  attachContainer({
+    namespaceName,
+    singletonStrategy: SingletonStrategy.oneInNamespace,
+    imageName: 'stavalfi/k8test-monitoring',
+  })
+
+export function driver() {
   const cleanups = cleanupAfterEach()
 
-  return {
-    cleanups,
-    startMonitorNamespace: (namespaceName: string) =>
-      execa.command(`node ${cliMonitoringPath} start-monitoring --local-image --namespace ${namespaceName}`, {
-        // eslint-disable-next-line no-process-env
-        env: { ...(process.env['DEBUG'] && { DEBUG: process.env['DEBUG'] }) },
-        stdio: 'inherit',
-      }),
-    registerNamespaceRemoval: (namespaceName: string) =>
-      cleanups.push(() =>
-        execa.command(`node ${cliMonitoringPath} delete-k8test-resources --namespace ${namespaceName}`, {
-          // eslint-disable-next-line no-process-env
-          env: { ...(process.env['DEBUG'] && { DEBUG: process.env['DEBUG'] }) },
-          stdio: 'inherit',
+  type SubscriptionOptions = SubscribeCreatorOptions & { manualUnsubscribe?: boolean }
+
+  return async function newEnv(subscriptionOptions: SubscriptionOptions[]) {
+    const namespaceName = randomNamespaceName()
+    const appId = randomAppId()
+
+    await startMonitorNamespace(namespaceName)
+
+    const subscriptions = await Promise.all(
+      subscriptionOptions.map(options =>
+        subscribe({
+          appId,
+          namespaceName,
+          isReadyPredicate: isServiceReadyPredicate,
+          containerOptions: { imagePullPolicy: 'Never' },
+          ...options,
         }),
       ),
-    attachMonitoringService: async (namespaceName: string) => {
-      const k8sClient = createK8sClient(ConnectionFrom.outsideCluster)
-      const socket = await attach({
-        k8sClient,
-        namespaceName,
-        singletonStrategy: SingletonStrategy.oneInNamespace,
-        imageName: 'stavalfi/k8test-monitoring',
-      })
-      cleanups.push(() => socket.terminate())
-    },
-    attachContainer: async ({
-      appId,
-      imageName,
-      namespaceName,
-      singletonStrategy,
-    }: {
-      namespaceName: string
-      appId?: string
-      imageName: string
-      singletonStrategy: SingletonStrategy
-    }) => {
-      const k8sClient = createK8sClient(ConnectionFrom.outsideCluster)
-      const socket = await attach({
-        k8sClient,
-        singletonStrategy,
-        appId,
-        imageName,
-        namespaceName,
-      })
-      cleanups.push(() => socket.terminate())
-    },
+    )
+
+    const manualUnsubscribes = subscriptionOptions
+      .map((options, i) => ({ options, i }))
+      .filter(({ options }) => !options.manualUnsubscribe)
+
+    cleanups.push(() => Promise.all(manualUnsubscribes.map(({ i }) => subscriptions[i].unsubscribe())))
+
+    cleanups.push(() => deleteK8testResources(namespaceName))
+
+    return {
+      subscriptions,
+      attachMonitoringService: async () => {
+        const socket = await attachMonitoringService(namespaceName)
+        cleanups.push(() => socket.terminate())
+      },
+      attachContainer: async (imageName: string, singletonStrategy: SingletonStrategy) => {
+        const socket = await attachContainer({
+          imageName,
+          singletonStrategy,
+          appId,
+          namespaceName,
+        })
+        cleanups.push(() => socket.terminate())
+      },
+    }
   }
 }
