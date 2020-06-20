162d857c39cb67a749bd9fe20328ee7a35c87b09
stavalfi <stavalfi@gmail.com>
2020-06-20 23:58:02 +0300

    wip

--- packages/scripts/src/ci/promote.ts
+++ packages/scripts/src/ci/promote.ts
@@ -17,6 +17,7 @@ export async function promote(orderedGraph: Graph<PackageInfo>): Promise<Package
     await Promise.all(
       toPromote.map(data => {
         const newVersion = data.target?.needPublish && data.target?.newVersion // it can't be false.
+        log(`promoting %s to version %s`, data.relativePackagePath, newVersion)
         execa.command(`yarn --cwd ${data.packagePath} version --no-git-tag-version ${newVersion}`, {
           stdio: 'inherit',
         })
