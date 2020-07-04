fe6651a354e6068edb13cf1a7346db0573ca8217
stavalfi <stavalfi@gmail.com>
2020-07-04 22:13:15 +0300

    wip

--- packages/ci/jest-global-setup.js
+++ packages/ci/jest-global-setup.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const execa = require('execa')
+const ciInfo = require('ci-info')
 
 module.exports = async () => {
   const command = `node --unhandled-rejections=strict ${require.resolve(
@@ -8,4 +10,10 @@ module.exports = async () => {
   await execa.command(command, {
     stdio: 'inherit',
   })
+
+  if (ciInfo.isCI) {
+    // in the tests of this package, we create git-repos and do commits so we need a git-user.
+    await execa.command(`git config --global user.email "test@test.com"`)
+    await execa.command(`git config --global user.name "test-user"`)
+  }
 }
