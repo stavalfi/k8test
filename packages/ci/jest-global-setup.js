49374a6f75e0e8dbc52bf83f9f978df2d627fe4d
stavalfi <stavalfi@gmail.com>
2020-07-04 17:50:09 +0300

    wip

--- packages/ci/jest-global-setup.js
+++ packages/ci/jest-global-setup.js
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const execa = require('execa')
+// const execa = require('execa')
 
 module.exports = async () => {
-  await execa.command(
-    `${require.resolve('k8test/dist/src/k8test-cli.js')} start-monitoring --local-image --namespace k8test-ci`,
-    {
-      stdio: 'inherit',
-    },
-  )
+  // await execa.command(
+  //   `${require.resolve('k8test/dist/src/k8test-cli.js')} start-monitoring --local-image --namespace k8test-ci`,
+  //   {
+  //     stdio: 'inherit',
+  //   },
+  // )
 }
