9a3093725cef9e096627815fb2c46bd5cb20225a
stavalfi <stavalfi@gmail.com>
2020-07-04 21:11:39 +0300

    wip

--- packages/ci/jest-global-setup.js
+++ packages/ci/jest-global-setup.js
@@ -2,9 +2,7 @@
 const execa = require('execa')
 
 module.exports = async () => {
-  const command = `${require.resolve(
-    'k8test/dist/src/k8test-cli.js',
-  )} start-monitoring --local-image --namespace k8test-ci`
+  const command = `${require.resolve('k8test/dist/src/index.js')} start-monitoring --local-image --namespace k8test-ci`
   // eslint-disable-next-line no-console
   console.log('stav1', `executing: "${command}"`)
   await execa.command(command, {
