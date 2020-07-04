bad95b2bb2c9b4f9b132daa88086934e92a48b2f
stavalfi <stavalfi@gmail.com>
2020-07-04 21:43:27 +0300

    wip

--- packages/ci/__tests__/pr-build.spec.ts
+++ packages/ci/__tests__/pr-build.spec.ts
@@ -1,21 +1,20 @@
-// import { newEnv } from './prepare-test'
-// import { TargetType } from './prepare-test/types'
+import { newEnv } from './prepare-test'
+import { TargetType } from './prepare-test/types'
 
-// const { createRepo } = newEnv()
+const { createRepo } = newEnv()
 
 test('1 package', async () => {
-  expect(1).toEqual(1)
-  // const { runCi } = await createRepo({
-  //   packages: [
-  //     {
-  //       name: 'a',
-  //       version: '1.0.0',
-  //       targetType: TargetType.npm,
-  //     },
-  //   ],
-  // })
-  // const pr = await runCi({
-  //   isMasterBuild: false,
-  // })
-  // expect(pr.published).toHaveProperty('size', 0)
+  const { runCi } = await createRepo({
+    packages: [
+      {
+        name: 'a',
+        version: '1.0.0',
+        targetType: TargetType.npm,
+      },
+    ],
+  })
+  const pr = await runCi({
+    isMasterBuild: false,
+  })
+  expect(pr.published).toHaveProperty('size', 0)
 })
